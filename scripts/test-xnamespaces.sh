a5b734c9f95a2d9049e70a584c8027864dd6358b
Alex Misstear <amisstea@redhat.com>
2025-03-06 11:17:07 -0500

    Sanitize remote namespace names for XNamespace resources
    Dots are allowed in k8s resource names with the exception of namespaces.
    The namespace will now be named after the composite resource rather than
    the managed Object resource (making it easier to map XNamespaces to
    remote namespaces) and replaces dots with hyphens.
    
    Signed-off-by: Alex Misstear <amisstea@redhat.com>

--- scripts/test-xnamespaces.sh
+++ scripts/test-xnamespaces.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/..
 
 kubectl apply -f $ROOT/examples/xnamespace
-kubectl wait --for=condition=Ready namespaces.eaas.konflux-ci.dev/my-namespace
-kubectl get secret/my-claim-secret
+kubectl wait --for=condition=Ready namespaces.eaas.konflux-ci.dev/my-namespace-1.0
+kubectl get secret/my-namespace-1.0-secret
 kubectl delete -f $ROOT/examples/xnamespace
 kubectl wait --for=delete objects --all --timeout=3m
